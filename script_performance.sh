de31ef3aae0396f34751585be2c72cacd4c371e5
sriram <sriram.katta@fau.de>
2024-07-31 14:17:17 +0200

    updated perf script to run on cluster

--- script_performance.sh
+++ script_performance.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -l
+#SBATCH --nodes=1 
+#SBATCH --partition singlenode
+#SBATCH --time=00:10:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=bmomptask
+#SBATCH --export=NONE
 
 module load intel likwid
 
